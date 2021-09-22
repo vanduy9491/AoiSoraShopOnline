2e2937da9c11155bd510d4208750b063f9e1976a
tran van duy <vanduy9491@gmail.com>
2021-09-22 10:16:55 +0700

    migration initial

---
+++ AoiSoraShop.Data/EF/AoiShopDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AoiSoraShop.Data.EF
+{
+    public class AoiShopDbContextFactory : IDesignTimeDbContextFactory<AoiShopDbContext>
+    {
+        public AoiShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+            var connectionString = configuration.GetConnectionString("AoiShopDB");
+            var optionsBuilder = new DbContextOptionsBuilder<AoiShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            return new AoiShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
